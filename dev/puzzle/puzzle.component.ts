804b7223acbe1b760501b3423f4809f6eb9d6a4a
Carlos Guisao Franco <titofranco@gmail.com>
2016-03-17 11:55:04 -0500

    Add bindings and css styles to puzzle.

--- dev/puzzle/puzzle.component.ts
+++ dev/puzzle/puzzle.component.ts
@@ -6,25 +6,33 @@ import {Component} from 'angular2/core';
         <section class="setup">
           <h2> Game setup </h2>
           Enter your name please:
-          <input type="text">
+          <input type="text" #name (keyup)="0">
         </section>
-        <section>
+        <section
+            [ngClass]="{
+             puzzle: true
+             solved: switch1.value == switch1Number
+             }"
+            [ngStyle]="{display: name.value === '' ? 'none' : 'block'}">
           <h2>The puzzle</h2>
         <p>Ok, Welcome <span class="name">XXX</span></p>
         <br>
         Switch1:
-        <input type="text"><br>
+        <input type="text" #switch1><br>
         Switch2:
-        <input type="text"><br>
+        <input type="text" #switch2><br>
         Switch3:
-        <input type="text"><br>
+        <input type="text" #switch3><br>
         Switch4:
-        <input type="text"><br>
+        <input type="text" #switch4><br>
         </section>
         <h2>Congratulations name, you did it!</h2>
         `
 })
 
 export class PuzzleComponent {
-
+    switch1Number:number;
+    switch2Number:number;
+    switch3Number:number;
+    switch4Number:number;
 }
