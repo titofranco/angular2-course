da6221812b11df1816e0e3fa91c38393eb055626
Carlos Guisao Franco <titofranco@gmail.com>
2016-03-17 11:37:43 -0500

    Add puzzle game basic layout.

--- dev/puzzle/puzzle.component.ts
+++ dev/puzzle/puzzle.component.ts
@@ -2,7 +2,27 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-puzzle',
-    template: `Puzzle component`,
+    template: `
+        <section class="setup">
+          <h2> Game setup </h2>
+          Enter your name please:
+          <input type="text">
+        </section>
+        <section>
+          <h2>The puzzle</h2>
+        <p>Ok, Welcome <span class="name">XXX</span></p>
+        <br>
+        Switch1:
+        <input type="text"><br>
+        Switch2:
+        <input type="text"><br>
+        Switch3:
+        <input type="text"><br>
+        Switch4:
+        <input type="text"><br>
+        </section>
+        <h2>Congratulations name, you did it!</h2>
+        `
 })
 
 export class PuzzleComponent {
